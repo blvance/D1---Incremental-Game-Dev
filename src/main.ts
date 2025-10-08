37d563dae554e756177ecf80ed89a3127a5c8742
blvance <blvance@ucsc.edu>
2025-10-08 16:16:29 +0000

    Added Graphic for button and created Title

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,23 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
+import exampleIconUrl from "./pooclick.png";
 import "./style.css";
 
+// Simple counter for demonstration
+let counter: number = 0;
+
+// Create basic HTML structure
 document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+  <h1>Dookie Clicker</h1>
+  <p>Counter: <span id="counter">0</span></p>
+  <button id="increment"><img src="${exampleIconUrl}" class="icon" /></button>
+  
+
 `;
+
+// Add click handler
+const button = document.getElementById("increment")!;
+const counterElement = document.getElementById("counter")!;
+
+button.addEventListener("click", () => {
+  counter += 1;
+  counterElement.textContent = `${counter}`;
+});
